0faf9526bdf33175c7edec96f527e8bdf0d917fc
Evan Wies <evan@neomantra.net>
2026-06-03 15:13:39 -0400

    logging: intercept exit/abort via ds4_abort_set hook and ensure log flushing
    Signed-off-by: Evan Wies <evan@neomantra.net>

--- ds4_stderr.h
+++ ds4_stderr.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +11,15 @@ extern "C" {
 
 void ds4_set_stderr(FILE *fp);
 void ds4_set_stderr_fd(int fd);
+
+#ifndef DS4_ABORT_FN_DEFINED
+#define DS4_ABORT_FN_DEFINED
+typedef void (*ds4_abort_fn)(void *ud, const char *msg);
+#endif
+void ds4_abort_set(ds4_abort_fn fn, void *ud);
+
+void ds4_abort_helper(const char *msg) __attribute__((noreturn));
+void ds4_exit_helper(int code) __attribute__((noreturn));
 
 static inline FILE *ds4_get_stderr_helper(void) {
     extern FILE *ds4_stderr;
@@ -19,6 +29,12 @@ static inline FILE *ds4_get_stderr_helper(void) {
 #ifndef DS4_STDERR_NO_REDEFINE
 #undef stderr
 #define stderr ds4_get_stderr_helper()
+
+#undef exit
+#define exit(code) ds4_exit_helper(code)
+
+#undef abort
+#define abort() ds4_abort_helper("ds4: process abort requested")
 #endif
 
 #ifdef __cplusplus
